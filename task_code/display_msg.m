56c9e9a5b5a7c6a3cd841bfa2c95a53436cd1d23
Su Jin An <sujinpatd@gmail.com>
2020-12-07 17:10:33 +0900

    merger forlder

--- task_code/display_msg.m
+++
@@ -1,9 +0,0 @@
-function [] = display_msg(msgString,xPos,yPos)
-    % Window Setting
-    global window;
-    white = WhiteIndex(window);
-    
-    % Display message String
-    StrBound = Screen('TextBounds',window,msgString);
-    DrawFormattedText(window, msgString, xPos-StrBound(3)./2,yPos, white);
-end
